980e3b6fa9586393874d651f3912c6b0d7be4211
Cagdas Enc <cagdas22edirne@gmail.com>
2020-11-10 16:26:11 -0500

    added LoginUtility

---
+++ src/LoginService.java
@@ -0,0 +1,9 @@
+public class LoginService {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello world");
+    }
+
+
+}
---
+++ src/LoginUtility.java
@@ -0,0 +1,9 @@
+public class LoginUtility {
+
+    public static void main(String[] args) {
+
+        // note
+
+    }
+
+}
